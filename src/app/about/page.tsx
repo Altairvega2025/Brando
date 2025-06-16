805c7d1abe3a9cbc406affb69b47a5ca3459d0e3
esther olubiyi <olubiyisther@gmail.com>
2025-06-16 10:45:52 +0100

    made the update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ import  {titleClassName,headerclass,What_We_Do, quote,textclass,We_work_with}  f
 
 export default function About() {
   return (
-   <div className="p-8 bg-[#f7f7f7] mt-0 md:mt-20">
+   <div className="p-8 bg-[#f7f7f7] mt-0 md:mt-20 mt-20">
       <h1 className="text-3xl font-bold text-black">About Us</h1>
       <p className='text-black'>Welcome to the About page.</p>
 
